4e87cababd8374dc2861e7a7571d1cc8d86b093c
cdavii <c.davieskeogh@gmail.com>
2021-12-15 15:34:08 +0000

    Initial commit of Printer class to deal with all output to the console.

---
+++ src/main/java/org/sparta/view/Printer.java
@@ -0,0 +1,19 @@
+package org.sparta.view;
+
+import org.sparta.model.sorters.Sortable;
+import java.util.Arrays;
+
+public class Printer {
+
+    public static void printMessage(String message) {
+        System.out.print(message);
+
+    }
+
+    public static void printInfo(Sortable sorter, int[] random) {
+        System.out.print(sorter.name() + " selected.\n" +
+                "Before sorting:\n" + Arrays.toString(random) +
+                "\nAfter sorting: \n" + Arrays.toString(sorter.sort(random)) +
+                "\nOperation took " + sorter.getTime() + " microseconds");
+    }
+}
