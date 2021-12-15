d66421883d2b7b8e26fb114a8269bc5a2695aba7
cdavii <c.davieskeogh@gmail.com>
2021-12-15 15:37:18 +0000

    Removed lines of code that were printing to console from within SortFactory and added method within each sorter to accomplish the same goal.

--- src/main/java/org/sparta/model/sorters/SortFactory.java
+++ src/main/java/org/sparta/model/sorters/SortFactory.java
@@ -8,15 +8,12 @@ public class SortFactory {
     public static Sortable getSorter(int selection){
         switch (selection) {
             case 1 -> {
-                System.out.println("Bubble sort selected");
                 return new BubbleSort();
             }
             case 2 -> {
-                System.out.println("Merge sort selected");
                 return new MergeSort();
             }
             case 3 -> {
-                System.out.println("Bubble sort selected");
                 return new BinaryTree();
             }
         }
